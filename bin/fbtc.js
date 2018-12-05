0488b914df775906e73b206add28c61427da086c
Judd <kepzorz@gmail.com>
2018-12-04 20:40:21 -0800

    pass btc deposit address privkey into createDepositTx

--- bin/fbtc.js
+++ bin/fbtc.js
@@ -33,19 +33,19 @@ async function main() {
 
   let gci = process.env.gci || TESTNET_GCI
   let client = await connect(gci)
-  let wallet = loadWallet(client)
+  let coinsWallet = loadWallet(client)
 
   let cmd = argv[0]
   if (cmd === 'balance' && argv.length === 1) {
     console.log(`
-Your address: ${wallet.address()}
-Your balance: ${await wallet.balance()}`)
+Your address: ${coinsWallet.address()}
+Your balance: ${await coinsWallet.balance()}`)
     process.exit()
   } else if (cmd === 'send' && argv.length === 3) {
     let recipientCoinsAddress = argv[1]
     let amount = Number(argv[2])
     try {
-      let result = await wallet.send(recipientCoinsAddress, amount)
+      let result = await coinsWallet.send(recipientCoinsAddress, amount)
       if (result.check_tx.code) {
         throw new Error(result.check_tx.log)
       }
@@ -58,14 +58,18 @@ Your balance: ${await wallet.balance()}`)
       process.exit(1)
     }
   } else if (cmd === 'deposit' && argv.length === 1) {
-    let DUMMY_PRIVKEY = randomBytes(32)
-    let btcDepositAddress = bitcoin.deriveBtcAddress(DUMMY_PRIVKEY)
-    // save private key in case user exits before deposit completes
+    let depositPrivateKey = generateSecpPrivateKey()
+    let btcDepositAddress = bitcoin.deriveBtcAddress(depositPrivateKey)
 
     let spinner = ora(
       `Waiting for Bitcoin deposit to ${btcDepositAddress}`
     ).start()
-    await doDepositProcess(btcDepositAddress)
+    await doDepositProcess(
+      depositPrivateKey,
+      btcDepositAddress,
+      client,
+      coinsWallet
+    )
   } else if (cmd === 'withdraw' && argv.length === 3) {
     let recipientBtcAddress = argv[1]
     let amount = Number(argv[2])
@@ -79,7 +83,7 @@ Your balance: ${await wallet.balance()}`)
 main()
 
 async function doDepositProcess(
-  privateKey,
+  depositPrivateKey,
   intermediateBtcAddress,
   client,
   coinsWallet
@@ -90,7 +94,7 @@ async function doDepositProcess(
   let depositUTXOs = await bitcoin.fetchUTXOs(intermediateBtcAddress)
   // build intermediate address -> signatories transaction
   let depositTransaction = bitcoin.createDepositTx(
-    intermediateBtcAddress.privateKey,
+    depositPrivateKey,
     validators,
     signatories,
     coinsWallet.address(),
