d46a1c8ab5fa38f1f3edc435823adc3e9930ff31
Judd <kepzorz@gmail.com>
2019-05-13 18:02:26 -0700

    update btc checkpoint

--- fullnode/app.js
+++ fullnode/app.js
@@ -45,17 +45,17 @@ let app = lotion({
 const trustedHeader = {
   version: 1073676288,
   prevHash: Buffer.from(
-    'a82fd47f65fc74d5ff947c71991c9cc4253ef7cd01e62dd6a700000000000000',
+    '08d61fcf532a044364f0648a41a55bba405d5aa0bf6f415d8402000000000000',
     'hex'
   ),
   merkleRoot: Buffer.from(
-    '07e9687977173526285defcdcc92f57defea0bd2b5236711f04565cf44daf454',
+    'a4fb1664d00ae4448dbdf8f99f1a78f7c5bb8036fd69d6f34aed5ee62386f65c',
     'hex'
   ),
-  timestamp: 1553416509,
-  bits: 436286314,
-  nonce: 2223907393,
-  height: 1485792
+  timestamp: 1556877853,
+  bits: 436373240,
+  nonce: 388744679,
+  height: 1514016
 }
 
 app.use(clauses)
